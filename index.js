e619cf36c05c526c2441c5a112a1796c84aa4683
Erich <eheiseler@gmail.com>
2022-03-01 20:40:58 -0500

    commit tree 7099ff69faf0884936148182fdde44ce81b86805

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    let form = document.querySelector('form')
+   return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+
+    })
+    .then(res => res.json())
+    .then(data => {
+      let userID = data.id
+      form.querySelector('span#userID').textContent = userID 
+    })
+    .catch(function (err) {
+        let errorMsg = err.message
+        form.querySelector('span#error').textContent = errorMsg
+    })
+}
+
